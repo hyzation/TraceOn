ee238c2a4666c68d785f7c2d5848c7eacc095a1c
何逸洲 <www.hyz978030720@sina.com>
2021-04-10 17:32:06 +0800

    d

--- pages/obelisk/index.js
+++ pages/obelisk/index.js
@@ -27,6 +27,9 @@ Page({
     longitude: null,
     latitude: null,
     distance: '',
+    markers: [],
+    quicklon: 0,
+    quicklai: 0,
   },
 
 
@@ -35,6 +38,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    //页面加载时获取当前位置
     var that = this
     wx.getLocation({
       type: 'wgs84',
@@ -55,19 +59,26 @@ Page({
 
   //点击地图上图标后触发事件
   bindmarkertap(e){
-    // console.log(e);
-  },
-  // 点击地图位置后触发事件
-  getMapPosition(e){
     console.log(e);
+    console.log(this.data.markers);
     var _this = this
+    this.data.markers.forEach(item => {
+      // console.log(Number(item.markerId))
+      // console.log(e.detail.markerId)
+      if(Number(item.markerId) == e.detail.markerId){
+        _this.setData({
+          quicklon: Number(item.longitude),
+          quicklai: Number(item.latitude)
+        })
+      }
+    })
     demo.direction({
       mode: 'walking',//可选值：'driving'（驾车）、'walking'（步行）、'bicycling'（骑行），不填默认：'driving',可不填
       //from参数不填默认当前地址
       //from: e.detail.value.start,
       to: {
-        longitude: e.detail.longitude,
-        latitude: e.detail.latitude,
+        longitude: _this.data.quicklon,
+        latitude: _this.data.quicklai,
       }, 
       success: function (res) {
         console.log(res);
@@ -105,6 +116,54 @@ Page({
       }
     });
   },
+  // 点击地图位置后触发事件
+  // getMapPosition(e){
+  //   console.log(e);
+  //   var _this = this
+  //   demo.direction({
+  //     mode: 'walking',//可选值：'driving'（驾车）、'walking'（步行）、'bicycling'（骑行），不填默认：'driving',可不填
+  //     //from参数不填默认当前地址
+  //     //from: e.detail.value.start,
+  //     to: {
+  //       longitude: e.detail.longitude,
+  //       latitude: e.detail.latitude,
+  //     }, 
+  //     success: function (res) {
+  //       console.log(res);
+  //       var ret = res;
+  //       var coors = ret.result.routes[0].polyline, pl = [];
+  //       //坐标解压（返回的点串坐标，通过前向差分进行压缩）
+  //       var kr = 1000000;
+  //       for (var i = 2; i < coors.length; i++) {
+  //         coors[i] = Number(coors[i - 2]) + Number(coors[i]) / kr;
+  //       }
+  //       //将解压后的坐标放入点串数组pl中
+  //       for (var i = 0; i < coors.length; i += 2) {
+  //         pl.push({ latitude: coors[i], longitude: coors[i + 1] })
+  //       }
+  //       console.log(pl)
+  //       //设置polyline属性，将路线显示出来,将解压坐标第一个数据作为起点
+  //       _this.setData({
+  //         latitude:pl[0].latitude,
+  //         longitude:pl[0].longitude,
+  //         polyline: [{
+  //           points: pl,
+  //           color: '#62a1c9',
+  //           width: 4
+  //         }]
+  //       })
+  //     },
+  //     fail: function (error) {
+  //       console.error(error);
+  //     },
+  //     complete: function (res) {
+  //       console.log(res);
+  //       _this.setData({
+  //         distance: res.result.routes[0].distance,
+  //       })
+  //     }
+  //   });
+  // },
 
   // 厕所位置
   getTheToilet() {
@@ -116,8 +175,8 @@ Page({
       keyword: '厕所',
       success: function (res) {
         console.log(res);
-        var mks = []
-        for (var i = 0; i < res.data.length; i++) {
+        var mks = []  //这里存放所有搜索到的地址信息的数组
+        for (let i = 0; i < res.data.length; i++) {
           mks.push({ // 获取返回结果，放到mks数组中
             //title: res.data[i].title,
             title: "香格里拉" + [i] + "号",
@@ -126,7 +185,8 @@ Page({
             longitude: res.data[i].location.lng,
             iconPath: "../../imgs/icon/toilet.png", //图标路径
             width: 20,
-            height: 20
+            height: 20,
+            markerId:90000000 + [i]
           })
         }
         _this.setData({ //设置markers属性，将搜索结果显示在地图中
