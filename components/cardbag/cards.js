ae86833b5d6bbeea3fa601308f3fe6ec9e151c75
unknown <hyz978030720@sina.com>
2022-03-29 18:13:59 +0800

    s

---
+++ components/cardbag/cards.js
@@ -0,0 +1,10 @@
+var cardlist = [
+  { id: '001', name: 'temp', img: '/imgs/icon/leterH.png', },
+  { id: '002', name: 'temp', img: '/imgs/icon/leterH.png', },
+  { id: '003', name: 'temp', img: '/imgs/icon/leterH.png', },
+]
+
+
+module.exports = {
+  cardlist
+}
