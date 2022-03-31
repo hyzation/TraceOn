dedc02fb197dd6280db6a3af20c97d57027480d6
unknown <hyz978030720@sina.com>
2022-03-31 16:00:11 +0800

    s

--- pages/sonofkami/test13/index.js
+++ pages/sonofkami/test13/index.js
@@ -13,7 +13,10 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    let that = this
+    wx.onUserCaptureScreen(function (res) {
+      that.canvasDraw()
+    })
   },
   canvasDraw() {
     let options = [
