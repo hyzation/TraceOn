929197cd53d85248bc68753405d389d43ba70981
unknown <hyz978030720@sina.com>
2022-06-01 17:41:21 +0800

    s

--- pages/sonofkami/test9/index.js
+++ pages/sonofkami/test9/index.js
@@ -7,7 +7,7 @@ function getOption(xData, data_cur, data_his) {
     backgroundColor: "#f5f4f3",
     color: ["#37A2DA", "#f2960d", "#67E0E3", "#9FE6B8"],
     title: {
-      text: '实时运行速度',
+      // text: '实时运行速度',
       textStyle: {
         fontWeight: '500',
         fontSize: 15,
@@ -16,14 +16,14 @@ function getOption(xData, data_cur, data_his) {
       x: 'center',
       y: '0'
     },
-    legend: {
-      data: ['今日', '昨日'],
-      right: 10
-    },
+    // legend: {
+    //   data: ['今日', '昨日'],
+    //   right: 10
+    // },
     grid: {
-      top: '15%',
+      top: '10%',
       left: '1%',
-      right: '3%',
+      right: '7%',
       bottom: '60rpx',
       containLabel: true
     },
@@ -47,31 +47,36 @@ function getOption(xData, data_cur, data_his) {
     },
     yAxis: {
       x: 'center',
-      name: 'km/h',
+      name: '￥',
       type: 'value',
-      min: 0,
-      max: 120
+      min: 10000,
+      max: 20000
     },
     series: [{
-      name: '今日',
+      name: '',
       zIndex: 2,
       type: 'line',
       smooth: true,
       symbolSize: 0,
       data: data_cur || []
-    }, {
-      name: '昨日',
-      zIndex: 1,
-      type: 'line',
-      smooth: true,
-      symbolSize: 0,
-      data: data_his || []
-    }]
+    }],
+    dataZoom: [
+      {
+        type: 'slider',//有单独的滑动条，用户在滑动条上进行缩放或漫游。inside是直接可以是在内部拖动显示
+        show: true,//是否显示 组件。如果设置为 false，不会显示，但是数据过滤的功能还存在。
+        start: 0,//数据窗口范围的起始百分比0-100
+        end: data_cur.length > 20 ? '50' : '100',//数据窗口范围的结束百分比0-100
+        xAxisIndex: [0],// 此处表示控制第一个xAxis，设置 dataZoom-slider 组件控制的 x轴 可是已数组[0,2]表示控制第一，三个；xAxisIndex: 2 ，表示控制第二个。yAxisIndex属性同理
+        bottom: 10 //距离底部的距离
+      },
+    ]
   };
   return option;
 }
 
 var chartLine = null;
+
+
 
 Page({
   data: {
@@ -92,31 +97,149 @@ Page({
         // chartLine.setOption(option);
         return chartLine
       }
-    }
+    },
+    showadd: false,
+    duration: 300,
+    position: 'right',
+    round: true,
+    overlay: true,
+    customStyle: '',
+    overlayStyle: 'background-color: rgba(39, 35, 35, 0.7); filter: blur(40px);',
   },
+
+  clickon(e) {
+    chartLine.on('click', function (params) {
+      // 控制台打印数据的名称
+      console.log(params.name);
+    });
+  },
+
+
 
   onReady: function () {
     let _this = this
-    var xData = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
-    var data_cur = [55, 67, 66, 78, 55, 67, 66, 78, 55, 67, 66, 78, 55, 67, 66, 78, 55, 67, 66, 78, 65, 66, 65, 54];
-    var data_his = [67, 66, 78, 65, 66, 65, 54, 67, 66, 78, 65, 66, 65, 54, 67, 66, 78, 65, 66, 65, 54, 67, 66, 78];
-    this.setData({
-      xData,
-      data_cur,
-      data_his
-    })
+    // var xData = [0];
+    // var data_cur = [100];
+    // // var data_his = [67, 66, 78, 65, 66, 65, 54, 67, 66, 78, 65, 66, 65, 54, 67, 66, 78, 65, 66, 65, 54, 67, 66, 78];
+    // this.setData({
+    //   xData,
+    //   data_cur,
+    //   // data_his
+    // })
+    this.getList()
     setTimeout(_this.oninit, 1000);
   },
 
+  // 渲染数据
   oninit(e) {
     console.log(chartLine);
+
     //ajax请求好数据后，调用获取option函数，传一些数据，
     //然后用全局变量echarts元素chartLine 来 setOption即可。
     // 三个参数： x轴数据，第一条线数据，第二条数据。 随意，echarts就跟正常用随便写就行
     // 随便写几个假数据
 
-    var option = getOption(this.data.xData, this.data.data_cur, this.data.data_his);
+    var option = getOption(this.data.xAxis, this.data.note);
     chartLine.setOption(option);
+  },
+
+  // 获取云函数数据
+  getList() {
+    let db = wx.cloud.database()
+    db.collection('fickgov')
+      .get()
+      .then(res => {
+        console.log(res.data);
+        let note = []
+        let xAxis = []
+        for (var i of res.data) {
+          note.push(i.note)
+          xAxis.push(i.xAxis)
+        }
+        console.log(note, xAxis);
+        this.setData({
+          note,
+          xAxis
+        })
+      })
+      .catch(err => {
+        console.log('获取信息失败')
+      })
+  },
+
+  // 新增提交
+  adddata(e) {
+    let notevalue = e.detail.value.note
+    let xAxisvalue = e.detail.value.xAxis
+    console.log(typeof notevalue, typeof xAxisvalue);
+    if (notevalue && xAxisvalue && notevalue >= 1000) {
+      wx.cloud.database().collection('fickgov')
+        .add({
+          data: {
+            note: notevalue,
+            xAxis: xAxisvalue,
+          }
+        })
+        .then(res => {
+          wx.showToast({
+            title: '添加成功',
+            icon: 'none',
+          })
+          this.setData({
+            notevalue: null,
+            xAxisvalue: null,
+            showadd: false,
+          })
+          //添加后，刷新数据
+          this.getList()
+        })
+        .catch(err => {
+          console.log('添加失败')
+        })
+    } else {
+      wx.showToast({
+        title: '请正确填写',
+        icon: 'none',
+      })
+    }
+  },
+
+  // 新增弹窗
+  addpage(e) {
+    const position = e.currentTarget.dataset.position
+    let customStyle = ''
+    let duration = this.data.duration
+    switch (position) {
+      case 'top':
+      case 'bottom':
+        customStyle = 'height: 30%;'
+        break
+      case 'right':
+        break
+    }
+    console.log(position, customStyle, duration);
+    this.setData({
+      position,
+      showadd: true,
+      customStyle,
+      duration
+    })
+  },
+
+  // 编辑
+  editpage(e) {
+    wx.showToast({
+      title: '正在开发...',
+      icon: 'none',
+    })
+  },
+
+  // 删除
+  delpage(e) {
+    wx.showToast({
+      title: '正在开发...',
+      icon: 'none',
+    })
   },
 
 
