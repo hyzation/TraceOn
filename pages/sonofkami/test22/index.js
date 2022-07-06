58259d76f80e989004593f500b1289370518c512
unknown <hyz978030720@sina.com>
2022-07-06 18:24:32 +0800

    s

--- pages/sonofkami/test22/index.js
+++ pages/sonofkami/test22/index.js
@@ -5,18 +5,22 @@ Page({
    * 页面的初始数据
    */
   data: {
-    obj: {
-      a: 1,
-      b: 2,
-      c: 3,
-    }
+    rechargemoneylist: [{ xibei: 10, money: 1, isselected: false, },
+    { xibei: 50, money: 5, isselected: false, },
+    { xibei: 450, money: 45, rewardxibei: 45, isselected: false, },
+    { xibei: 880, money: 88, rewardxibei: 88, isselected: false, },
+    { xibei: 1980, money: 198, rewardxibei: 198, isselected: false, },
+    ],
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
-
+    let rechargemoneylist = this.data.rechargemoneylist
+    let a =415
+    if (rechargemoneylist.filter(item => item.money == a).length != 0) {
+      console.log(111);
+    }
   },
 
   /**
